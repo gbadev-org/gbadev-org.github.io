c7975afb8112c2bdfb2946fbe78ea914e0e93fcc
Antonio Vivace <avivace4@gmail.com>
2023-02-20 11:16:47 +0100

    sidebar: add events

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,65 @@
-import { defaultTheme } from '@vuepress/theme-default'
+import { defaultTheme } from "@vuepress/theme-default";
 
 export default {
-  lang: 'en-US',
-  title: 'gbadev',
-  description: 'Game Boy Advance development resources',
-  plugins: [
-    '@vuepress/plugin-palette',
-    { preset: 'scss' },
-  ],
+  lang: "en-US",
+  title: "gbadev",
+  description: "Game Boy Advance development resources",
+  plugins: ["@vuepress/plugin-palette", { preset: "scss" }],
   theme: defaultTheme({
-    logo: 'https://forum.gbadev.net/static/logo.png',
+    logo: "https://forum.gbadev.net/static/logo.png",
     sidebar: [
-      'README.md',
-      '/gbajam22.md',
+      "README.md",
+      "/resources.md",
+      {
+        text: "GBA Doc",
+        link: "https://gbadev.net/gbadoc/",
+      },
+      {
+        text: "Events",
+        children: [
+          // SidebarItem
+          {
+            text: "GBA Winter Jam 2023",
+            link: "https://itch.io/jam/gba-winter-jam-2023",
+          },
+          {
+            text: "GBA Winter Jam 2023",
+            link: "https://itch.io/jam/gba-winter-jam-2023",
+          },
+          "/gbajam22.md",
+          {
+            text: "GBA Winter Jam 2021",
+            link: "https://itch.io/jam/gba-winter-jam-2021",
+          },
+          {
+            text: "GBA Jam 2021",
+            link: "https://itch.io/jam/gbajam21",
+          },
+        ],
+      },
+      {
+        text: "GBA Homebrew Games",
+        link: "https://hh.gbdev.io/games/gba",
+      },
     ],
+    contributors: false,
     navbar: [
       {
-        text: 'GitHub',
-        link: 'https://github.com/gbdev',
+        text: "GitHub",
+        link: "https://github.com/gbadev-org",
       },
       {
-        text: 'Patreon',
-        link: 'https://www.patreon.com/gbdev01',
+        text: "OpenCollective",
+        link: "https://opencollective.com/gbadev",
       },
       {
-        text: 'Discord',
-        link: 'https://discord.io/gbadev',
+        text: "Discord",
+        link: "https://discord.io/gbadev",
       },
       {
-        text: 'Forum',
-        link: 'https://forum.gbadev.net',
-      }
+        text: "Forum",
+        link: "https://forum.gbadev.net",
+      },
     ],
   }),
-}
+};

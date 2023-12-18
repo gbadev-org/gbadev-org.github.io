cb8df204f44d0ee1cc295827a5e1d8d27b377a47
Antonio Vivace <avivace4@gmail.com>
2023-12-18 23:22:32 +0100

    add matomo tarcking code

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,20 @@ export default {
   lang: "en-US",
   title: "gbadev",
   description: "Game Boy Advance development resources",
+  head: [
+    ['script', {}, `
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//stats.gbdev.io/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '3']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+  `],
   plugins: ["@vuepress/plugin-palette", { preset: "scss" }],
   theme: defaultTheme({
     logo: "https://forum.gbadev.net/static/logo.png",
